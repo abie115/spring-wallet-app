d649a892a632450517aaea57c56cf9a001c169e3
abie115 <donicczka@gmail.com>
2019-03-18 01:10:23 +0100

    some changes

---
+++ src/main/java/com/abiewska/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.abiewska.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Witaj!");
+		model.addAttribute("tagline", "Twoja kieszonka");
+		return "welcome";
+	}
+}
